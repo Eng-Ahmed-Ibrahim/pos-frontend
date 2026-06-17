bca260692ddabc450413169bc335948a89df247a
Ahmed Ebrahim <your_github_email@example.com>
2026-06-18 02:26:10 +0300

    upload updates

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
     const token = localStorage.getItem("token");
-
+    // test
     return token ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
